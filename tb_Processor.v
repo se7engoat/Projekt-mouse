60d5a762f21e3fcd6a89edeaa1ef3bf46fe795b0
Sriram Jagathisan <s2736992@tlf29.see.ed.ac.uk>
2025-03-11 20:13:00 +0000

    processor testbench structure added

--- tb_Processor.v
+++ tb_Processor.v
@@ -25,6 +25,45 @@ module tb_Processor;
         .BUS_INTERRUPTS_ACK(BUS_INTERRUPTS_ACK)
     );
     
+
+    //this can be a task that can be used for maybe a load and store function
+    //LOAD instruction
+    task readFromMemory(int A, int B);
+    begin
+        #20 BUS_DATA = 8'h01;
+        #20 BUS
+    end
+
+    //STORE instruction
+    task writeToMemory(int A, int B);
+    begin
+        
+    end
+
+    //Arithmetic instruction
+    task mathOP(int A. int B, int opCode);
+    begin
+        
+    end
+
+    //Jump Instruction
+    task jumpInstruction(int Addr, int returnContext);
+    begin
+        
+    end
+
+    //Branch Instruction
+    task branchInstr(int Addr, int Context);
+    begin
+        
+    end
+
+    //Dereference
+    task Dereference(int A, int B);
+    begin
+        
+    end
+
     
     always #10 CLK = ~CLK;
     
